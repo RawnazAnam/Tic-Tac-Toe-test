5b6a14beb701d7723ddeee14fc69b2973fd4e06f
RawnazAnam <rawnaz.anam@temple.edu>
2019-10-22 18:28:37 -0400

    Test

---
+++ Lab06_TicTacToe/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06_TicTacToe
+{
+    public class TicTacToe
+    {
+        char[,] playerBoard;
+        int j = 0;
+       // int i = 0;
+        char[] player = new char[2];
+        char currentPlayer;
+            public TicTacToe()
+        {
+            player[0] = 'x';
+            player[1] = 'o';
+            currentPlayer = player[0];
+            CreateBoard();
+        }
+        public int CreateBoard()
+        {
+             playerBoard = new char[3, 3];//creates a 3d character array
+            j = playerBoard.Length;
+            return j;
+        }
+        public char CurrentPlayer()
+        {
+            return currentPlayer;
+        }
+        public char ChangePlayer()
+        {
+            if (currentPlayer == 'x')
+            {
+                currentPlayer = player[1];
+                return currentPlayer;
+            }
+            else
+            {
+                currentPlayer = player[0];
+                return currentPlayer;
+            }
+        }
+        public void MakeMove(int a, int b)
+        {
+            
+            playerBoard[a, b] = currentPlayer;
+        }
+        public Boolean SpaceInUse(int a,int b)
+        {
+            char c = playerBoard[a,b];
+            if (c == 'x')
+            {
+                return true;
+            }
+            else if (c == 'o')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        
+    }
+}
---
+++ TicTacToeTest/TicTacToeTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab06_TicTacToe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06_TicTacToe.Tests
+{
+    [TestClass()]
+    public class TicTacToeTests
+    {
+        TicTacToe tt = new TicTacToe();
+        [TestMethod()]
+        public void CreateBoardTest()
+        {
+            int j = tt.CreateBoard();
+            Assert.AreEqual(9, j);
+        }
+
+        [TestMethod()]
+        public void CurrentPlayerTest()
+        {
+            char j = tt.CurrentPlayer();
+
+        }
+
+        [TestMethod()]
+        public void ChangePlayerTest()
+        {
+            char j = tt.ChangePlayer();
+            Assert.AreEqual('o', j);
+        }
+
+        [TestMethod()]
+        public void MakeMoveTest()
+        {
+            tt.MakeMove(0, 0);
+            Boolean j = tt.SpaceInUse(0, 0);
+
+            Assert.IsTrue(j);
+        }
+
+        [TestMethod()]
+        public void SpaceInUseTest()
+        {
+            Boolean j = tt.SpaceInUse(0, 0);
+
+            //Assert.IsTrue(j);
+            Assert.IsFalse(j);
+        }
+    }
+}
